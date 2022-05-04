43877d78f6f6ac5092785774e1c07cc2dd713087
DanaGhaddar <dana.ghaddar@lau.edu>
2022-05-04 22:25:56 +0300

    finalizing

--- app/src/main/java/com/example/cosmetics_final_project/Login.java
+++ app/src/main/java/com/example/cosmetics_final_project/Login.java
@@ -68,7 +68,8 @@ public class Login extends AppCompatActivity {
         tosignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+Intent intnt=new Intent(getApplicationContext(),Signup.class);
+startActivity(intnt);
 
             }
         });
