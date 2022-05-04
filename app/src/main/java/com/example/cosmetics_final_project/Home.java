6a8e8637a71bdd6c7601494d397be9d32cee06c0
DanaGhaddar <dana.ghaddar@lau.edu>
2022-05-04 21:31:15 +0300

    Designing

--- app/src/main/java/com/example/cosmetics_final_project/Home.java
+++ app/src/main/java/com/example/cosmetics_final_project/Home.java
@@ -34,8 +34,6 @@ public class Home extends AppCompatActivity {
 
 
 
-
-
         profilebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
