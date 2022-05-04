69bdd7f3047714b52db9374548c1b5f20d446bfc
DanaGhaddar <dana.ghaddar@lau.edu>
2022-05-04 18:13:38 +0300

    itempage

---
+++ app/src/main/java/com/example/cosmetics_final_project/item_details.java
@@ -0,0 +1,14 @@
+package com.example.cosmetics_final_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class item_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_details);
+    }
+}
