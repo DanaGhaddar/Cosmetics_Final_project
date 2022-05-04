1f71376677431de026a533f1f866f12d5da9e699
DanaGhaddar <dana.ghaddar@lau.edu>
2022-05-04 21:13:31 +0300

    design modifiying

--- app/src/main/java/com/example/cosmetics_final_project/Home.java
+++ app/src/main/java/com/example/cosmetics_final_project/Home.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class Home extends AppCompatActivity {
-    ImageView profilebtn;
+    ImageView profilebtn,image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
         profilebtn=(ImageView) findViewById(R.id.imageView19);
+
+         image = (ImageView)findViewById(R.id.imageView13);
+        image.setX(-1000);
+        image.animate().translationXBy(1000).rotation(3600).setDuration(2000);
+
+
+
+
         profilebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
